866d5d8a024fa7aec663a908575db9d68af3622d
eminkesgin <muhammeteminkesgin@gmail.com>
2022-05-17 23:06:25 +0300

    initial commit

---
+++ src/test/java/com/natay/saysthat/SaysThatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.natay.saysthat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaysThatApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
